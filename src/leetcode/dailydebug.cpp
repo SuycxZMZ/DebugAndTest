6cd1d57150bfde0213edb2dc614d157ddfb9d131
yuansu <suycx65@163.com>
2024-06-21 22:36:41 +0800

    日常测试

--- src/leetcode/dailydebug.cpp
+++ src/leetcode/dailydebug.cpp
@@ -7,42 +7,53 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <algorithm>
+
 using namespace std;
 
 class Solution {
 public:
-    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) 
-    {
-        int n = customers.size();
-        int cus_cnt = 0;
-        for (int i = 0; i < n; ++i)
-        {
-            if (grumpy[i] == 0) cus_cnt += customers[i];
-        }
-        // cout << "cus_cnt : " << cus_cnt << endl;
-        int l = 0, r = 0, ans = 0;
-        for ( ; r < n && l <= r; ++l)
-        {
-            while (r < n && r - l < minutes - 1)
-            {
-                if (grumpy[r] == 1) cus_cnt += customers[r];
-                ++r;
+    bool exist(vector<vector<char>>& board, string word) {
+        vector<vector<int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int m = board.size(), n = board[0].size();
+        if (word.size() > m * n) return false;
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+        function<bool(int, int, int)> dfs = [&](int idx, int x, int y) -> bool {
+            if (board[x][y] != word[idx] || idx >= word.size()) return false;
+            if (board[x][y] == word[idx] && idx == word.size() - 1) return true;
+            bool isvalid = false;
+            int next_x = 0, next_y = 0;
+            for (int i = 0; i < 4; ++i) {
+                if (isvalid) break;
+                next_x = x + dir[i][0];
+                next_y = y + dir[i][1];
+                if (next_x < 0 || next_x >= m || next_y < 0 || next_y >= n ||
+                    vis[next_x][next_y])
+                    continue;
+                vis[next_x][next_y] = true;
+                isvalid = dfs(idx + 1, next_x, next_y) || isvalid;
+                vis[next_x][next_y] = false;
             }
-            cout << cus_cnt << " " << "r:" << r << " ";
-            ans = ans > cus_cnt ? ans : cus_cnt;
-            if (grumpy[l] == 1) cus_cnt -= customers[l];
-            cout << "debug:" << cus_cnt << " " << "l" << l << endl;
+            return isvalid;
+        };
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (board[i][j] == word[0]) {
+                    if (dfs(0, i, j)) {
+                        std::cout << i << " " << j;
+                        return true;
+                    }
+                }
+            }
         }
-        return ans;
+        return false;
     }
 };
 
 
 int main()
 {
-    vector<int> customs = {1,0,1,2,1,1,7,5};
-    vector<int> gromy = {0,1,0,1,0,1,0,1};
-    Solution s_s;
-    int res = s_s.maxSatisfied(customs, gromy, 3);
+    vector<vector<char>> bord = {{'a', 'a'}, {'A', 'A'}};
+    bool res = Solution().exist(bord, "aaa");
+    std::cout << res << std::endl;
     return 0;
 }
