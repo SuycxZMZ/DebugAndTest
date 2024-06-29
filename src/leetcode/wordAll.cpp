809b6b94c381daaf2992c83e1032dad0a4aff00e
yuansu <suycx65@163.com>
2024-06-29 10:53:52 +0800

    添加：最长回文子串，中心扩散和动态规划解法

--- src/leetcode/wordAll.cpp
+++ src/leetcode/wordAll.cpp
@@ -31,6 +31,54 @@ public:
         }
         return f[s.size()];
     }
+
+    // 最长回文子串
+    // 中心扩散，从每一个位置开始，向左右各自扩散到与当前位置不想等，然后比较左右，直到再次不想等
+    string longestPalindrome(string s) {
+        if (s.size() <= 1) return s;
+        int sl = s.size();
+        int l = 0, r = 0, len = 1, maxStart = 0, maxLen = 0;
+        // 枚举中心位置
+        for (int i = 0; i < sl; ++i) {
+            l = i - 1, r = i + 1; // 左右指针位置
+            while (l >= 0 && s[l] == s[i]) {
+                ++len;
+                --l;
+            }
+            while (r < sl && s[r] == s[i]) {
+                ++len;
+                ++r;
+            }
+            while (l >= 0 && r < sl && s[l] == s[r]) {
+                len += 2;
+                --l; ++r;
+            }
+            if (len > maxLen) {
+                maxLen = len;
+                maxStart = l;
+            }
+            len = 1;
+        }
+        return s.substr(maxStart + 1, maxLen);
+    }
+    // dp，记录状态 f[l][r] 表示 l-->r 是否回文
+    string longestPalindrome2(string s) {
+        if (s.size() <= 1) return s;
+        int sl = s.size(), maxStart = 0, maxLen = 1;
+        vector<vector<bool>> f(sl, vector<bool>(sl, false));
+        for (int r = 1; r < sl; ++r) {
+            for (int l = 0; l < r; ++l) {
+                if (s[l] == s[r] && (r - l <= 2 || f[l + 1][r - 1])) {
+                    f[l][r] = true;
+                    if (r - l + 1 > maxLen) {
+                        maxLen = r - l + 1;
+                        maxStart = l;
+                    }
+                }
+            }
+        }
+        return s.substr(maxStart, maxLen);
+    }
 };
 
 int main() {
