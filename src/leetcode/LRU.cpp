f9a0225fee90e1267f3975ddb19896b3cf6c6cb2
SuycxZMZ <suycx65@163.com>
2024-07-24 17:52:58 +0800

    add LFU

--- src/leetcode/LRU.cpp
+++ src/leetcode/LRU.cpp
@@ -3,47 +3,117 @@
 //
 #include <list>
 #include <iostream>
+#include <unordered_map>
+
+using namespace std;
 
 /**
  * @brief LRUCache
  * @brief get put O(1)
  */
 class LRUCache {
-private:
-    int m_capacity = 0;
-    std::list<std::pair<int, int>> m_list;
-    std::unordered_map<int, std::list<std::pair<int, int>>::iterator> m_map;
 public:
-    LRUCache(int capacity) : m_capacity(capacity)
+    using PII = pair<int, int>;
+private:
+    int _capacity = 0;
+    std::list<PII> _list;
+    unordered_map<int, std::list<PII>::iterator> _map;
+public:
+    LRUCache(int capacity) : _capacity(capacity)
     {
     }
 
     int get(int key) {
-        auto it = m_map.find(key);
-        if (m_map.end() == it) return -1;
-        // 更新list
-        int ans = it->second->second;
-        m_list.erase(it->second);
-        m_list.emplace_front(key, ans);
-        // 更新map
-        m_map[key] = m_list.begin();
-        return ans;
+        auto it = _map.find(key);
+        if (_map.end() == it) {
+            return -1;
+        } else { // 找到了，要把这个 移到链表前面
+            int val = it->second->second;
+            _list.erase(it->second);
+            _list.emplace_front(key, val);
+            it->second = _list.begin();
+            return val;
+        }
     }
 
     void put(int key, int value) {
-        auto it = m_map.find(key);
-        if (m_map.end() == it) {
-            if (m_map.size() == m_capacity) {
-                m_map.erase(m_list.back().first);
-                m_list.pop_back();
+        auto it = _map.find(key);
+        if (_map.end() == it) { // 没找到
+            if (_map.size() == _capacity) { // 要移除链表内的最后一个元素
+                _map.erase(_list.back().first);
+                _list.pop_back();
             }
-            m_list.emplace_front(key, value);
-            m_map.emplace(key, m_list.begin());
-        } else {
-            m_list.erase(it->second);
-            m_list.emplace_front(key, value);
-            m_map[key] = m_list.begin();
+            _list.emplace_front(key, value); // 在前面添加，最近刚使用过
+            _map.emplace(key, _list.begin());
+        } else { // 找到了，那么就要往前移，并且把value改掉
+            _list.erase(it->second);
+            _list.emplace_front(key, value);
+            it->second = _list.begin();
         }
+    }
+};
+
+
+class LFUCache {
+private:
+    int _capacity;
+    int _minFreq;
+    using ListIter = std::list<pair<int, int>>::iterator;
+    // 存缓存的键值对    (K, (V, 位置))
+    unordered_map<int, pair<int, ListIter>> _keyTable;
+    // 存储每个频率对应的双向链表  (频率 --> list<K, V>)
+    unordered_map<int, list<pair<int, int>>> _freqTable;
+    // 存储每个元素对应的频率
+    unordered_map<int, int> _freq;
+
+public:
+    LFUCache(int capacity) : _capacity(capacity), _minFreq(0) {
+    }
+    
+    int get(int key) {
+        auto it = _keyTable.find(key);
+        if (_keyTable.end() == it) return -1;
+
+        // 最近使用了，要增加该key的频率
+        int val = _keyTable[key].first;
+        int f = _freq[key];
+        _freqTable[f].erase(_keyTable[key].second); // 要增加，就先移除
+
+        if (_freqTable[f].empty()) {
+            _freqTable.erase(f);
+            if (_minFreq == f) ++_minFreq;  // 试图更新一下最小使用频率
+        }
+
+        ++_freq[key];
+        _freqTable[f + 1].emplace_front(key, val);
+        _keyTable[key].second = _freqTable[f + 1].begin();
+        return val;
+    }
+    
+    void put(int key, int value) {
+        if (_capacity == 0) return;
+        // 已经有了
+        if (_keyTable.find(key) != _keyTable.end()) {
+            _keyTable[key].first = value;
+            get(key);
+            return;
+        }
+
+        // 已经满了，要删除一个
+        if (_keyTable.size() == _capacity) {
+            int eK = _freqTable[_minFreq].back().first;
+            _freqTable[_minFreq].pop_back();
+            if (_freqTable[_minFreq].empty()) _freqTable.erase(_minFreq);
+            // 开始删
+            _keyTable.erase(eK);
+            _freq.erase(eK);
+        }
+
+        // 插入,新的，频率要等于1
+        _minFreq = 1;
+        _freq[key] = 1;
+        _freqTable[1].emplace_front(key, value);
+        _keyTable[key] = {value, _freqTable[1].begin()};
     }
 };
 
