917c635076dea3662b03e93ed3398fbf0f72e3b4
SuycxZMZ <suycx65@163.com>
2024-07-26 13:25:09 +0800

    modify

--- src/leetcode/dailydebug.cpp
+++ src/leetcode/dailydebug.cpp
@@ -1,89 +1,59 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
 
 using namespace std;
 
-template <typename T, typename Compare = std::less<T>>
-class Heap {
+class Solution {
 public:
-    Heap(Compare comp = Compare()) : comp(comp) {}
-
-    void push(const T& value);
-    void pop();
-    const T& top() const;
-    bool empty() const { return data.empty(); }
-    size_t size() const { return data.size(); }
-
-private:
-    std::vector<T> data;
-    Compare comp;
-
-    // 用于在插入新元素后，维护堆的性质。
-    void heapifyUp(size_t index);
-    // 用于在删除堆顶元素后，维护堆的性质。
-    void heapifyDown(size_t index);
+    bool exist(vector<vector<char>>& board, string word) {
+        vector<vector<int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int m = board.size(), n = board[0].size();
+        if (word.size() > m * n) return false;
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+        function<bool(int, int, int)> dfs = [&](int idx, int x, int y) -> bool {
+            if (board[x][y] != word[idx] || idx >= word.size()) return false;
+            if (board[x][y] == word[idx] && idx == word.size() - 1) return true;
+            bool isvalid = false;
+            int next_x = 0, next_y = 0;
+            for (int i = 0; i < 4; ++i) {
+                if (isvalid) break;
+                next_x = x + dir[i][0];
+                next_y = y + dir[i][1];
+                if (next_x < 0 || next_x >= m || next_y < 0 || next_y >= n ||
+                    vis[next_x][next_y])
+                    continue;
+                vis[next_x][next_y] = true;
+                isvalid = dfs(idx + 1, next_x, next_y) || isvalid;
+                vis[next_x][next_y] = false;
+            }
+            return isvalid;
+        };
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (board[i][j] == word[0]) {
+                    if (dfs(0, i, j)) {
+                        std::cout << i << " " << j;
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 };
 
-template <typename T, typename Compare>
-void Heap<T, Compare>::push(const T& value) {
-    data.push_back(value);
-    heapifyUp(data.size() - 1);
-}
 
-template <typename T, typename Compare>
-void Heap<T, Compare>::pop() {
-    if (data.empty()) {
-        throw std::out_of_range("Heap is empty");
-    }
-    data[0] = data.back();
-    data.pop_back();
-    heapifyDown(0);
-}
-
-template <typename T, typename Compare>
-const T& Heap<T, Compare>::top() const {
-    if (data.empty()) {
-        throw std::out_of_range("Heap is empty");
-    }
-    return data[0];
-}
-
-template <typename T, typename Compare>
-void Heap<T, Compare>::heapifyUp(size_t index) {
-    while (index > 0) {
-        size_t parent = (index - 1) / 2;
-        if (!comp(data[index], data[parent])) {
-            break;
-        }
-        std::swap(data[index], data[parent]);
-        index = parent;
-    }
-}
-
-template <typename T, typename Compare>
-void Heap<T, Compare>::heapifyDown(size_t index) {
-    size_t size = data.size();
-    while (true) {
-        size_t left = 2 * index + 1;
-        size_t right = 2 * index + 2;
-        size_t smallest = index;
-
-        if (left < size && comp(data[left], data[smallest])) {
-            smallest = left;
-        }
-        if (right < size && comp(data[right], data[smallest])) {
-            smallest = right;
-        }
-        if (smallest == index) {
-            break;
-        }
-        std::swap(data[index], data[smallest]);
-        index = smallest;
-    }
-}
-
-
-int main() {
-
-    
+int main()
+{
+    vector<vector<char>> bord = {{'a', 'a'}, {'A', 'A'}};
+    bool res = Solution().exist(bord, "aaa");
+    std::cout << res << std::endl;
     return 0;
 }
